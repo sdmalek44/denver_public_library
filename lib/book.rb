5efdda5a387113b7c65b17234f831890b9d7cf50
Stephen Malek <malek.stephen44@gmail.com>
2018-05-24 09:25:36 -0600

    finish iteration 1 and test

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,13 @@
 class Book
+  attr_reader :author_last_name,
+              :author_first_name,
+              :title,
+              :publication_date
 
-
+  def initialize(book_hash)
+    @author_first_name = book_hash[:author_first_name]
+    @author_last_name = book_hash[:author_last_name]
+    @title = book_hash[:title]
+    @publication_date = book_hash[:publication_date][-4..-1]
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,31 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
+  def test_it_exists
+    b = Book.new({author_first_name: "Harper", author_last_name: "Lee",
+       title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
+    assert_instance_of Book, b
+  end
 
+  def test_access_author
+    b = Book.new({author_first_name: "Harper", author_last_name: "Lee",
+       title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "Harper", b.author_first_name
+    assert_equal "Lee", b.author_last_name
+  end
+
+  def test_access_title
+    b = Book.new({author_first_name: "Harper", author_last_name: "Lee",
+       title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_equal "To Kill a Mockingbird", b.title
+  end
+
+  def test_access_publication_date
+    b = Book.new({author_first_name: "Harper", author_last_name: "Lee",
+       title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_equal "1960", b.publication_date
+  end
 end
