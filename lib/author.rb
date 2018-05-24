24064f55b3b1664cda4bdd9dbaa56b2aa57a205b
Stephen Malek <malek.stephen44@gmail.com>
2018-05-24 09:43:14 -0600

    finish second iteration

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,16 @@
-class Author
+require './lib/book'
 
+class Author
+  attr_reader :books
+
+  def initialize(name_hash)
+    @first_name = name_hash[:first_name]
+    @last_name = name_hash[:last_name]
+    @books = []
+  end
+
+  def add_book(title, date)
+    @books << Book.new({author_first_name: @first_name, author_last_name: @last_name,
+       title: title, publication_date: date})
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,25 @@
 require './test/test_helper'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
+  def test_it_exists
+    bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
+    assert_instance_of Author, bronte
+  end
 
+  def test_it_adds_books
+    bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal [], bronte.books
+
+    bronte.add_book("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, bronte.books[0]
+
+    bronte.add_book("Villette", "1853")
+
+    assert_equal 'Jane Eyre', bronte.books[0].title
+    assert_equal 'Villette', bronte.books[1].title
+  end
 end
